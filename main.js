5c3d78c0e90112841031e8fe7bf015c198ce3590
shaurya2712 <iamshauryamalhotra@gmail.com>
2022-08-03 06:00:00 +0530

    some optimizations and added js file

---
+++ main.js
@@ -0,0 +1,41 @@
+var typedAbout = new Typed(".auto-type-about", {
+  strings: ["Shaurya", "Web Developer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+var typedSkills = new Typed(".auto-type-skill1", {
+  strings: ["HTML,"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: false,
+});
+
+var typedSkills = new Typed(".auto-type-skill2", {
+  strings: ["CSS, "],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: false,
+});
+
+var typedSkills = new Typed(".auto-type-skill3", {
+  strings: ["JAVASCRIPT, "],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: false,
+});
+
+var typedSkills = new Typed(".auto-type-skill4", {
+  strings: ["BOOTSTRAP "],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: false,
+});
+
+var typedHobby = new Typed(".auto-type-hobby", {
+  strings: ["Photography", "Mtb-Rider", "Sketching"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
